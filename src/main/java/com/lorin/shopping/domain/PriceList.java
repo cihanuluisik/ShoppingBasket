1885eb0d131e701e17a2b717d886f4bd82a3efa3
cihanuluisik <cihanuluisik@hotmail.com>
2017-07-30 13:56:50 +0100

    Cihan : tiny refactoring

--- src/main/java/com/lorin/shopping/domain/PriceList.java
+++ src/main/java/com/lorin/shopping/domain/PriceList.java
@@ -3,13 +3,14 @@ package com.lorin.shopping.domain;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class PriceList {
 
     private final Map<String, Integer> priceMap = new HashMap<>();
 
     public PriceList(List<ItemPrice> itemPrices) {
-        if ( itemPrices ==null || itemPrices.isEmpty() ||itemPrices.stream().anyMatch( itm -> itm == null) ){
+        if ( itemPrices ==null || itemPrices.isEmpty() ||itemPrices.stream().anyMatch(Objects::isNull) ){
             throw new IllegalArgumentException("Item prices can not be empty");
         }
         itemPrices.forEach(itemPrice -> priceMap.put(itemPrice.getName(), itemPrice.getPrice()));
